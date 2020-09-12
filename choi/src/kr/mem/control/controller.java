ac6c6a0e2d739f5c0009648ba897af54643ee371
kms1572 <67312577+kms1572@users.noreply.github.com>
2020-09-12 16:08:29 +0900

    1

---
+++ choi/src/kr/mem/control/controller.java
@@ -0,0 +1,19 @@
+package kr.mem.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/controller")
+public class controller extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//test
+	}
+
+}
